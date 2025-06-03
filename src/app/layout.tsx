bc3293daed61157a988a83f88c09e160ed9d1d5f
KVBautista-cs <kevinbautista383@gmail.com>
2025-06-03 15:14:24 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
-import Head from 'next/head'; // ✅ Add this
+import Head from 'next/head'; 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
